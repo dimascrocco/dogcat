76dafd7979c3a1cb5828cc45bf10b038152c64a6
dimas.crocco@gmail.com <dimas.crocco@gmail.com>
2016-06-14 03:39:30 -0300

    utilizando score do knn

--- analisador_tabajara.py
+++ analisador_tabajara.py
@@ -12,6 +12,8 @@ import mir3.modules.features.flatness as flatn
 import mir3.modules.features.low_energy as lowen
 
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.cross_validation import train_test_split
+
 import glob
 
 def feature(sound, feature, plot=False, show=False, xlabel="Tempo (s)", ylabel=""):
@@ -37,18 +39,19 @@ def vetor(data):
     #return  [np.min(data), np.max(data), np.mean(data), np.var(data)] # len(data)]
     return  [np.mean(data), np.var(data)] # len(data)]
 
-def processar(target, features):
+def processar(target, features, plot=False):
+    #print "- ", target
     wav2spec = spec.Wav2Spectrogram() # Objeto que converte arquivos wav para espectrogramas
     som = wav2spec.convert(open(target, 'rb'), window_length=1024, window_step=512, spectrum_type='magnitude')
 
     ro=fe=ce=en=ft=le=None
     f = features
-    if f[0]: ro = feature(som, roll.Rolloff(), plot=True, ylabel="Rolloff"); 
-    if f[1]: fe = feature(som, specfl.Flux(), plot=True, ylabel="Spectral Flux"); 
-    if f[2]: ce = feature(som, cent.Centroid(), plot=True, ylabel="Centroid"); 
-    if f[3]: en = feature(som, energ.Energy(), plot=True, ylabel="Energy"); 
-    if f[4]: ft = feature(som, flatn.Flatness(), plot=True, ylabel="Flatness"); 
-    if f[5]: le = feature(som, lowen.LowEnergy(), plot=True, ylabel="Low Energy"); 
+    if f[0]: ro = feature(som, roll.Rolloff(), plot=plot, ylabel="Rolloff"); 
+    if f[1]: fe = feature(som, specfl.Flux(), plot=plot, ylabel="Spectral Flux"); 
+    if f[2]: ce = feature(som, cent.Centroid(), plot=plot, ylabel="Centroid"); 
+    if f[3]: en = feature(som, energ.Energy(), plot=plot, ylabel="Energy"); 
+    if f[4]: ft = feature(som, flatn.Flatness(), plot=plot, ylabel="Flatness"); 
+    if f[5]: le = feature(som, lowen.LowEnergy(), plot=plot, ylabel="Low Energy"); 
 
     result = []
     if ro: vetor_ro = vetor(ro.data); result += vetor_ro
@@ -60,22 +63,41 @@ def processar(target, features):
 
     return result
 
+def plot_spread(x_gatos, x_caes, title):
+    mean_gatos = [v[0] for v in x_gatos]
+    var_gatos = [v[1] for v in x_gatos]
+    mean_caes = [v[0] for v in x_caes]
+    var_caes = [v[1] for v in x_caes]
 
-# SCRIPT #
+    plt.figure()
+    plt.title(spread_title)
+    plt.scatter(mean_gatos, var_gatos, color='red');
+    plt.scatter(mean_caes, var_caes, color='blue');
+    plt.ylabel('Media');
+    plt.xlabel('Variancia');
+    plt.legend(['Gatos', 'Caes'], loc=4);
+    plt.show()
+
+
+
+
+# CABINE DE CONTROLE... nao pressione o botao vermelho!
 gatos = glob.glob("db/cats/*.wav")
 caes = glob.glob("db/dogs/*.wav")
+PLOT_MULTIPLO = False # plota todas as amostras de uma vez
 
-# Features a serem utilizadas
-f_rolloff = 1
+# Features a serem utilizadas (utilize uma por vez, multiplas podem exibir graficos sobrepostos)
+f_rolloff = 0
 f_spectral_flux = 0
 f_centroid = 0
 f_energy = 0
 f_flatness = 0
-f_low_energy = 0
+f_low_energy = 1
 
 features = [f_rolloff, f_spectral_flux, f_centroid, f_energy, f_flatness, f_low_energy]
 features_labels = ['Rolloff','Spectral Flux','Centroid','Energy','Flatness','Low Energy']
 
+# TODO da pra fazer isso em uma linha? converter lista em mapa indexado?
 spread_title = []
 count = 0
 for v in features:
@@ -84,85 +106,70 @@ for v in features:
     count += 1
 spread_title = ' + '.join(spread_title)
 
-print spread_title 
 
+# resultados
 n_gatos = len(gatos)
 n_caes = len(caes)
-
-# valores
 X_gatos = []
 X_caes = []
+y_gatos = [] 
+y_caes= [] 
 
-# labels
-y = [] 
 
-
-print "# GATOS"
-for som in gatos[0:n_gatos]: #/2]:
-    print som
-    result = processar(som, features)
+print "* processando gatos..."
+for som in gatos[0:n_gatos]:
+    result = processar(som, features, plot=PLOT_MULTIPLO)
     X_gatos.append(result)
-    y.append('G')
+    y_gatos.append('G')
 
-plt.show()
-plt.clf()
+if PLOT_MULTIPLO:
+    plt.show()
+    plt.clf()
 
-print "# CAES"
-for som in caes[0:n_caes]: #/2]:
-    print som
-    result = processar(som, features)
+print "* processando caes..."
+for som in caes[0:n_caes]:
+    result = processar(som, features, plot=PLOT_MULTIPLO)
     X_caes.append(result)
-    y.append('C')
+    y_caes.append('C')
 
-plt.show()
-PLOT_SPREAD = True
-if PLOT_SPREAD:
-    mean_gatos = [v[0] for v in X_gatos]
-    var_gatos = [v[1] for v in X_gatos]
-    mean_caes = [v[0] for v in X_caes]
-    var_caes = [v[1] for v in X_caes]
+# one big list to bind them all...
+X = X_gatos + X_caes
+y = y_gatos + y_caes
 
-    plt.figure()
-    plt.title(spread_title)
-    plt.scatter(mean_gatos, var_gatos, color='red');
-    plt.scatter(mean_caes, var_caes, color='blue');
-    plt.ylabel('Media');
-    plt.xlabel('Variancia');
-    #plt.xlim([60, 120]);
-    #plt.ylim([1.6, 2.2]);
-    plt.legend(['Gatos', 'Caes'], loc=4);
+if PLOT_MULTIPLO:
     plt.show()
 
+if 1 > 2:
+    plot_spread(X_gatos, X_caes, spread_title)
 
 
-X = X_gatos + X_caes
-i = 0
-print "label feature-vector"
-for r in X:
-    print y[i], r # label, feature vector
-    i += 1
+#i = 0
+#print "label feature-vector"
+#for r in X:
+#    print y[i], r # label, feature vector
+#    i += 1
 
-#print "Global min/max:", np.min(m), np.max(M)
-#print np.unique(map(len, X))
 
 neigh = KNeighborsClassifier(n_neighbors=3); # n_neighbors = K
-neigh.fit(X_gatos + X_caes, y)
 
-acertos_gatos = 0
-acertos_caes = 0
+dados_treino, dados_teste, label_treino, label_teste = train_test_split(X, y, train_size=.5)
+#neigh.fit(X_gatos + X_caes, y_gatos + y_caes)
+neigh.fit(dados_treino, label_treino)
+score = neigh.score(dados_teste, label_teste)
+print "% acertos: ", 100*score
 
-print "# testes"
-print "# gatos"
-for som in gatos[n_gatos/2:n_gatos]:
-    #print som
-    result = processar(som, features)
-    print "--> ", neigh.predict([result])
-
-print "# caes"
-for som in caes[n_caes/2:n_caes]:
-    #print som
-    result = processar(som, features)
-    print "--> ", neigh.predict([result])
+#print "# testes"
+#print "# gatos"
+#for som in gatos[n_gatos/2:n_gatos]:
+#    #print som
+#    result = processar(som, features, plot=False)
+#    print "--> ", neigh.predict([result])
+#
+#print "# caes"
+#for som in caes[n_caes/2:n_caes]:
+#    #print som
+#    result = processar(som, features, plot=False)
+#    print "--> ", neigh.predict([result])
 
 
     
